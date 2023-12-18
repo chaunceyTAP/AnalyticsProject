3751f9d6a4bd8d7dd3af865006c178dd103def31
chaunce_dev <chaunceyplum@gmail.com>
2023-12-18 10:48:01 -0500

    added DATA LAYER

--- unsubscribe.js
+++ unsubscribe.js
@@ -1,14 +1,31 @@
 const axios = require('axios')
+
 const options = {
   method: 'POST',
-  url: 'https://api.zetaglobal.net/ver2/partner-dev-tap-cxm/subscribers/chaunceyp%40tapcxm.com/unsubscribe',
+  url: 'https://api.zetaglobal.net/ver2/partner-dev-tap-cxm/subscribers/identify',
   headers: {
     accept: 'application/json',
     Accept: 'application/json',
     'content-type': 'application/json',
     authorization: 'Basic YXBpOmIwODY2ODljZTAyYTg3Nzg1MDI0NGFmMWUzNWU5MDZi',
   },
-  data: { source: 'api', message_uid: '1700159102345e32c4885bcc8' },
+  data: {
+    subscriber: {
+      uid: 'chaunceyp@tapcxm.com2',
+      email: 'chaunceyp@tapcxm.com',
+      contacts: {
+        contact_type: 'email',
+        preferences: ['standard'],
+        subscription_status: 'active',
+        contact_value: 'chaunceyp@tapcxm.com',
+      },
+      source: 'api',
+
+      first_name: 'Chauncey',
+      last_name: 'Plummer',
+      name: 'Chauncey Plummer',
+    },
+  },
 }
 
 axios
